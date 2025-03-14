0bd3792da9fca4272ae3e7396610d00cf0cb1d41
CamiloEscar <camiloescar1995@gmail.com>
2025-03-13 23:43:06 -0300

    Variables dinamicas en formulario

--- src/app/modules/categories/create-categorie/create-categorie.component.ts
+++ src/app/modules/categories/create-categorie/create-categorie.component.ts
@@ -1,18 +1,49 @@
 import { Component } from '@angular/core';
+import { CategoriesService } from '../service/categories.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-create-categorie',
   templateUrl: './create-categorie.component.html',
-  styleUrls: ['./create-categorie.component.scss']
+  styleUrls: ['./create-categorie.component.scss'],
 })
 export class CreateCategorieComponent {
-
   type_categorie: number = 1;
-  processFile($event: any) {
 
+  name: string = '';
+  icon: string = '';
+  position: number = 1;
+  categorie_second_id: string = '';
+
+  categorie_third_id: string = '';
+
+  imagen_previsualizacion: any =
+    'https://preview.keenthemes.com/metronic8/demo1/assets/media/svg/illustrations/easy/2.svg';
+
+  file_imagen: any = null;
+
+  constructor(
+    public categorieService: CategoriesService,
+    public toast: ToastrService
+  ) {}
+  processFile($event: any) {
+    if ($event.target.files[0].type.indexOf('imagen') < 0) {
+      this.toast.error('El archivo seleccionado no es una imagen', 'Error');
+      return;
+    }
+    this.file_imagen = $event.target.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(this.file_imagen);
+    reader.onloadend = () => {
+      this.imagen_previsualizacion = reader.result;
+    };
   }
 
-  changeTypeCategorie(val:number){
+  changeTypeCategorie(val: number) {
     this.type_categorie = val;
+  }
+
+  save() {
+
   }
 }
--- src/app/modules/categories/service/categories.service.ts
+++ src/app/modules/categories/service/categories.service.ts
@@ -1,9 +1,85 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject, finalize } from 'rxjs';
+import { AuthService } from '../../auth';
+import { URL_SERVICIOS } from 'src/app/config/config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoriesService {
+  isLoading$: Observable<boolean>;
+  isLoadingSubject: BehaviorSubject<boolean>;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient, //nos permite hacer peticiones http definir la ruta, enviar datos y recibir datos de la api
+    public authservice: AuthService //podemos obtener el token del usuario identificado
+  ) {
+    this.isLoadingSubject = new BehaviorSubject<boolean>(false);
+    this.isLoading$ = this.isLoadingSubject.asObservable();
+  }
+
+  listCategories(search: string) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.authservice.token,
+    });
+    let URL = URL_SERVICIOS + '/admin/categories';
+    return this.http
+      .get(URL, { headers: headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+  configCategories() {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.authservice.token,
+    });
+    let URL = URL_SERVICIOS + '/admin/categories/config';
+    return this.http
+      .get(URL, { headers: headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+  createCategories(data: any) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.authservice.token,
+    });
+    let URL = URL_SERVICIOS + '/admin/categories';
+    return this.http
+      .post(URL, data, { headers: headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+
+  showCategorie(categorie_id: string) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.authservice.token,
+    });
+    let URL = URL_SERVICIOS + '/admin/categories/' + categorie_id;
+    return this.http
+      .get(URL, { headers: headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+
+  updateCategories(data: any, categorie_id: string) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.authservice.token,
+    });
+    let URL = URL_SERVICIOS + '/admin/categories/' + categorie_id;
+    return this.http
+      .post(URL, data, { headers: headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+
+  deleteCategorie(categorie_id: string) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.authservice.token,
+    });
+    let URL = URL_SERVICIOS + '/admin/categories/' + categorie_id;
+    return this.http
+      .delete(URL, { headers: headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
 }
