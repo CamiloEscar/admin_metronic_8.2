19e1f002e3299d3374f6c20006a3653dbe2a0169
CamiloEscar <camiloescar1995@gmail.com>
2025-03-18 13:41:06 -0300

    Registro, Edicion y eliminacion de un atributo

--- src/app/modules/attributes/delete-attribute/delete-attribute.component.ts
+++ src/app/modules/attributes/delete-attribute/delete-attribute.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AttributesService } from '../service/attributes.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-delete-attribute',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class DeleteAttributeComponent {
 
+  @Input () attribute:any;
+
+  @Output() AttributeD: EventEmitter<any> = new EventEmitter();
+  isLoading:any;
+  constructor(
+    public attributeService: AttributesService,
+    public toastr: ToastrService,
+    public modal: NgbActiveModal
+  ){
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.isLoading = this.attributeService.isLoading$
+  }
+  delete() {
+    this.attributeService.deleteAttributes(this.attribute.id).subscribe((resp:any) => {
+      this.AttributeD.emit({message: 200});
+      this.modal.close();
+    })
+  }
 }
