36cbe5d7a763452b1e3f044734a2f90b0c6b27cc
CamiloEscar <camiloescar1995@gmail.com>
2025-03-22 15:08:41 -0300

    Correccion para el registro del producto

--- src/app/modules/products/create-product/create-product.component.ts
+++ src/app/modules/products/create-product/create-product.component.ts
@@ -26,16 +26,16 @@ export class CreateProductComponent {
   categorie_first_id: any = [];
   categorie_second_id: any = [];
   categorie_third_id: any = [];
-
   categorie_first: any = [];
   categorie_seconds: any = [];
-  categorie_seconds_backups: any = [];
   categorie_thirds: any = [];
+  categorie_seconds_backups: any = [];
   categorie_thirds_backups: any = [];
 
   dropdownList: any = [];
-  selectedItems: any = [];
+  selectedItemsTags: any = [];
   dropdownSettings: IDropdownSettings = {};
+  word: string = '';
 
   isShowMultiselect: boolean = false;
 
@@ -49,17 +49,17 @@ export class CreateProductComponent {
     //Add 'implements OnInit' to the class.
     this.isLoading$ = this.productService.isLoading$;
 
-    this.dropdownList = [
-      { item_id: 1, item_text: 'Mumbai' },
-      { item_id: 2, item_text: 'Bangaluru' },
-      { item_id: 3, item_text: 'Pune' },
-      { item_id: 4, item_text: 'Navsari' },
-      { item_id: 5, item_text: 'New Delhi' },
-    ];
-    this.selectedItems = [
-      { item_id: 3, item_text: 'Pune' },
-      { item_id: 4, item_text: 'Navsari' },
-    ];
+    // this.dropdownList = [
+    //   { item_id: 1, item_text: 'Mumbai' },
+    //   { item_id: 2, item_text: 'Bangaluru' },
+    //   { item_id: 3, item_text: 'Pune' },
+    //   { item_id: 4, item_text: 'Navsari' },
+    //   { item_id: 5, item_text: 'New Delhi' },
+    // ];
+    // this.selectedItems = [
+    //   { item_id: 3, item_text: 'Pune' },
+    //   { item_id: 4, item_text: 'Navsari' },
+    // ];
     this.dropdownSettings = {
       singleSelection: false,
       idField: 'item_id',
@@ -69,16 +69,30 @@ export class CreateProductComponent {
       // itemsShowLimit: 3,
       allowSearchFilter: true,
     };
+
+    this.configAll();
+  }
+
+  configAll() {
+    this.productService.configAll().subscribe((resp: any) => {
+      console.log(resp);
+      this.marcas = resp.brands;
+      this.categorie_first = resp.categorie_first;
+      this.categorie_seconds = resp.categorie_seconds;
+      this.categorie_thirds = resp.categorie_thirds;
+    });
   }
 
   addItems() {
     this.isShowMultiselect = true;
-    this.dropdownList.push({ item_id: 6, item_text: 'Item 6' });
-    this.selectedItems.push({ item_id: 6, item_text: 'Item 6' });
+    let time_date = new Date().getTime();
+    this.dropdownList.push({ item_id: time_date, item_text: this.word });
+    this.selectedItemsTags.push({ item_id: time_date, item_text: this.word });
     setTimeout(() => {
+      this.word = ''; // Limpia el campo de entrada
       this.isShowMultiselect = false;
       this.isLoadingView();
-    }, 100)
+    }, 100);
   }
   processFile($event: any) {
     const file = $event.target.files[0];
@@ -90,6 +104,7 @@ export class CreateProductComponent {
     }
 
     this.file_imagen = file;
+    console.log(this.file_imagen);
 
     // Leer el archivo y convertirlo en una URL para la previsualización
     const reader = new FileReader();
@@ -98,6 +113,10 @@ export class CreateProductComponent {
       this.isLoadingView();
     };
     reader.readAsDataURL(file);
+  }
+
+  onDescriptionChange(event: any) {
+    this.description = event.editor.getData();
   }
 
   isLoadingView() {
@@ -109,14 +128,19 @@ export class CreateProductComponent {
 
   changeDepartamento() {
     this.categorie_seconds_backups = this.categorie_seconds.filter(
-      (item: any) => item.categorie_second_id == this.categorie_third_id
+      (item: any) => item.categorie_second_id == this.categorie_first_id
     );
+    // Reset
+    this.categorie_second_id = '';
+    this.categorie_thirds_backups = [];
+    this.categorie_third_id = '';
   }
 
   changeCategorie() {
     this.categorie_thirds_backups = this.categorie_thirds.filter(
       (item: any) => item.categorie_second_id == this.categorie_second_id
     );
+    this.categorie_third_id = '';
   }
 
   onItemSelect(item: any) {
@@ -127,29 +151,64 @@ export class CreateProductComponent {
   }
 
   save() {
-    if (!this.title || !this.file_imagen) {
+    if (
+      !this.title ||
+      !this.sku ||
+      !this.price_ars ||
+      !this.price_usd ||
+      !this.marca_id ||
+      !this.file_imagen ||
+      !this.categorie_first_id.length ||
+      !this.description ||
+      !this.resumen ||
+      this.selectedItemsTags === 0
+    ) {
       this.toast.error('Todos los campos son obligatorios', 'Error');
       return;
     }
 
     let formData = new FormData();
     formData.append('title', this.title);
-
+    formData.append('sku', this.sku);
+    formData.append('price_ars', this.price_ars + '');
+    formData.append('price_usd', this.price_usd + '');
+    formData.append('brand_id', this.marca_id);
     formData.append('portada', this.file_imagen);
+    formData.append('categorie_first_id', this.categorie_first_id);
+    if (this.categorie_second_id) {
+      formData.append('categorie_second_id', this.categorie_second_id);
+    }
+    if (this.categorie_third_id) {
+      formData.append('categorie_third_id', this.categorie_third_id);
+    }
+    formData.append('description', this.description);
+    formData.append('resumen', this.resumen);
+    formData.append('multiselect', JSON.stringify(this.selectedItemsTags));
 
     this.productService.createProducts(formData).subscribe((resp: any) => {
       console.log(resp);
 
       if (resp.message === 403) {
-        this.toast.error('No tiene permisos para crear categorias', 'Error');
+        this.toast.error('No tiene permisos para crear productos', 'Error');
         return;
-      }
+      } else {
+        this.title = '';
+        this.sku = '';
+        this.price_ars = 0;
+        this.price_usd = 0;
+        this.marca_id = '';
+        this.categorie_first_id = '';
+        this.categorie_second_id = '';
+        this.categorie_third_id = '';
+        this.file_imagen = null;
+        this.description = '';
+        this.resumen = '';
+        this.selectedItemsTags = [];
 
-      (this.title = ''),
-        (this.file_imagen = null),
         (this.imagen_previsualizacion =
           'https://preview.keenthemes.com/metronic8/demo1/assets/media/svg/illustrations/easy/2.svg'),
-        this.toast.success('El producto creada exitosamente', 'Éxito');
+          this.toast.success('El producto creada exitosamente', 'Éxito');
+      }
     });
   }
 }
