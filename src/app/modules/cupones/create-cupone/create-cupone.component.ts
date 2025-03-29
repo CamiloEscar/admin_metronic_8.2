1e1d7123366b6bad2e63b6341de5752da9e5561a
CamiloEscar <camiloescar1995@gmail.com>
2025-03-29 17:40:33 -0300

    Dinamismo para el formulario de registro de un cupon

--- src/app/modules/cupones/create-cupone/create-cupone.component.ts
+++ src/app/modules/cupones/create-cupone/create-cupone.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CuponesService } from '../service/cupones.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-create-cupone',
@@ -9,27 +11,38 @@ export class CreateCuponeComponent {
   code: any;
   isLoading$: any;
   type_discount: number = 1;
-  discount:number = 0;
-  type_count:number = 1;
-  type_cupone:number = 1;
-  product_id:any;
-  categorie_id:any;
-  brand_id:any;
-  num_use:number = 0;
+  discount: number = 0;
+  type_count: number = 1;
+  type_cupone: number = 1;
+  product_id: any;
+  categorie_id: any;
+  brand_id: any;
+  num_use: number = 0;
 
-  categories_add:any = [];
-  products_add:any = [];
-  brands_add:any = [];
+  categories_add: any = [];
+  products_add: any = [];
+  brands_add: any = [];
 
-  categories_first:any = [];
-  products:any = [];
-  brands:any = [];
+  categories_first: any = [];
+  products: any = [];
+  brands: any = [];
 
-  constructor() {}
+  constructor(
+    public cuponesService: CuponesService,
+    public toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
+
+    this.isLoading$ = this.cuponesService.isLoading$;
+
+    this.cuponesService.configCupones().subscribe((resp: any) => {
+      this.categories_first = resp.categories;
+      this.products = resp.products;
+      this.brands = resp.brands;
+    });
   }
 
   changeTypeDiscount(value: number) {
@@ -40,20 +53,104 @@ export class CreateCuponeComponent {
   }
   changeTypeCupone(value: number) {
     this.type_cupone = value;
+    //limpiar campos
+    this.products_add = [];
+    this.categories_add = [];
+    this.brands_add = [];
+    this.product_id = null;
+    this.categorie_id = null;
+    this.brand_id = null;
   }
-
 
   save() {}
 
-  removeProduct(product:any){
+  addProduct() {
+    if (!this.product_id) {
+      this.toastr.error('Validacion', 'Es necesario seleccionar un producto');
+      return;
+    }
+    let INDEX = this.products_add.findIndex(
+      (prod: any) => prod.id == this.product_id
+    );
+    if (INDEX != -1) {
+      this.toastr.error(
+        'Validacion',
+        'Este producto ya se encuentra en la lista'
+      );
+      return;
+    }
 
+    let DATA = this.products.find(
+      (product: any) => product.id == this.product_id
+    );
+    if (DATA) {
+      this.products_add.push(DATA);
+    }
+  }
+  addCategorie() {
+    if (!this.categorie_id) {
+      this.toastr.error('Validacion', 'Es necesario seleccionar una categoria');
+      return;
+    }
+    let INDEX = this.categories_add.findIndex(
+      (prod: any) => prod.id == this.categorie_id
+    );
+    if (INDEX != -1) {
+      this.toastr.error(
+        'Validacion',
+        'Esta categoria ya se encuentra en la lista'
+      );
+      return;
+    }
+
+    let DATA = this.categories_first.find(
+      (categorie: any) => categorie.id == this.categorie_id
+    );
+    if (DATA) {
+      this.categories_add.push(DATA);
+    }
+  }
+  addBrand() {
+    if (!this.brand_id) {
+      this.toastr.error('Validacion', 'Es necesario seleccionar una marca');
+      return;
+    }
+    let INDEX = this.brands_add.findIndex(
+      (prod: any) => prod.id == this.brand_id
+    );
+    if (INDEX != -1) {
+      this.toastr.error('Validacion', 'Esta marca ya se encuentra en la lista');
+      return;
+    }
+
+    let DATA = this.brands.find((brand: any) => brand.id == this.brand_id);
+    if (DATA) {
+      this.brands_add.push(DATA);
+    }
   }
 
-  removeCategorie(categorie:any){
-
+  removeProduct(product: any) {
+    let INDEX = this.products_add.findIndex(
+      (prod: any) => prod.id == product.id
+    );
+    if (INDEX != -1) {
+      this.products_add.splice(INDEX, 1);
+    }
   }
 
-  removeBrand(brand:any){
+  removeCategorie(categorie: any) {
+    let INDEX = this.categories_add.findIndex(
+      (prod: any) => prod.id == categorie.id
+    );
+    if (INDEX != -1) {
+      this.categories_add.splice(INDEX, 1);
+    }
+  }
 
+  removeBrand(brand: any) {
+    let INDEX = this.brands_add.findIndex((prod: any) => prod.id == brand.id);
+    if (INDEX != -1) {
+      this.brands_add.splice(INDEX, 1);
+    }
   }
 }
--- src/app/modules/cupones/service/cupones.service.ts
+++ src/app/modules/cupones/service/cupones.service.ts
@@ -1,9 +1,85 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject, finalize } from 'rxjs';
+import { URL_SERVICIOS } from 'src/app/config/config';
+import { AuthService } from '../../auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CuponesService {
+isLoading$: Observable<boolean>;
+  isLoadingSubject: BehaviorSubject<boolean>;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient, //nos permite hacer peticiones http definir la ruta, enviar datos y recibir datos de la api
+    public authservice: AuthService //podemos obtener el token del usuario identificado
+  ) {
+    this.isLoadingSubject = new BehaviorSubject<boolean>(false);
+    this.isLoading$ = this.isLoadingSubject.asObservable();
+  }
+
+  listCupones(page:number = 1 ,search: string) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.authservice.token,
+    });
+    let URL = URL_SERVICIOS + "/admin/cupones?page="+page+"&search="+search;
+    return this.http
+      .get(URL, { headers: headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+  configCupones() {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.authservice.token,
+    });
+    let URL = URL_SERVICIOS + '/admin/cupones/config';
+    return this.http
+      .get(URL, { headers: headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+  createCupones(data: any) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.authservice.token,
+    });
+    let URL = URL_SERVICIOS + '/admin/cupones';
+    return this.http
+      .post(URL, data, { headers: headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+
+  showCupone(cupone_id: string) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.authservice.token,
+    });
+    let URL = URL_SERVICIOS + '/admin/cupones/' + cupone_id;
+    return this.http
+      .get(URL, { headers: headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+
+  updateCupones(data: any, cupone_id: string) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.authservice.token,
+    });
+    let URL = URL_SERVICIOS + '/admin/cupones/' + cupone_id;
+    return this.http
+      .post(URL, data, { headers: headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
+
+  deleteCupone(cupone_id: string) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.authservice.token,
+    });
+    let URL = URL_SERVICIOS + '/admin/cupones/' + cupone_id;
+    return this.http
+      .delete(URL, { headers: headers })
+      .pipe(finalize(() => this.isLoadingSubject.next(false)));
+  }
 }
